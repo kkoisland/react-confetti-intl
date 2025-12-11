b7873a576661305ef920b5a42ce3916e0dbb3337
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2025-12-11 11:14:11 -0800

    feat: setup react-intl with i18n directory structure

---
+++ src/i18n/index.ts
@@ -0,0 +1,3 @@
+export { IntlProvider, useLocaleContext } from './IntlProvider';
+export { SUPPORTED_LOCALES, LOCALE_NAMES } from './types';
+export type { Locale } from './types';
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { IntlProvider } from "./i18n/IntlProvider.tsx";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
@@ -9,7 +10,9 @@ if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
 			<BrowserRouter>
-				<App />
+				<IntlProvider>
+					<App />
+				</IntlProvider>
 			</BrowserRouter>
 		</StrictMode>,
 	);
