e57db6c7fca526593316848caaeed44b9feb2f79
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2026-01-03 12:13:05 -0800

    ci: setup GitHub Pages deployment with SPA routing support

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
-			<BrowserRouter>
+			<BrowserRouter basename="/react-confetti-intl">
 				<IntlProvider>
 					<App />
 				</IntlProvider>
